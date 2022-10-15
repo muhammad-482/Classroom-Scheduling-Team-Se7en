a615d2597b7dde31656eac33b19f9a19f299bf68
muhammad-482 <79003762+muhammad-482@users.noreply.github.com>
2022-10-15 15:28:05 +0200

    changed admin route
    changed admin route to be in the index page

--- hack1/urls.py
+++ hack1/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("algorithm.urls")),
+    path('', admin.site.urls),
+    path('hello/', include("algorithm.urls")),
     
 ]
